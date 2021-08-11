7883e10f340ebe880dcb78e1939fa5b9c8a22312
elena <lenok.ok.lol@gmail.com>
2021-08-11 14:43:58 +0300

    added README

--- download.py
+++ download.py
@@ -1,8 +1,45 @@
+from os import name
 import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urlparse, urlsplit, urljoin
+import argparse
+
+
+def parse_cli_args():
+    parser = argparse.ArgumentParser(description='Download books')
+    parser.add_argument('start_id', help='set start book id', type=int)
+    parser.add_argument('end_id', help='set end book id', type=int)
+    args = parser.parse_args()
+
+    return args
+
+
+def parse_book_page(page):
+    book_info = {}
+    soup = BeautifulSoup(page, 'lxml')
+    title_and_author = soup.find('h1').text
+    book_info['title'] = title_and_author.split('::')[0].strip()
+    book_info['author'] = title_and_author.split('::')[1].strip()
+
+    genres = soup.find('span', class_='d_book').find_all('a')
+    genres_list = []
+    for genre in genres:
+        genres_list.append(genre.text)
+    book_info['genre'] = genres_list
+
+    img_url = soup.find(class_='bookimage').find('img')['src']
+    book_info['img_url'] = img_url
+
+    comments = soup.find_all('div', class_='texts')
+    comments_list = []
+    for comment in comments:
+        comment_text = comment.find('span', class_='black').text
+        comments_list.append(comment_text)
+    book_info['comments'] = comments_list
+
+    return book_info
 
 
 def check_for_redirect(response):
@@ -45,7 +82,8 @@ def download_image(url, folder='images'):
     
     return filepath
 
-for i in range(1, 11):
+args = parse_cli_args()
+for i in range(args.start_id, args.end_id + 1):
     url = 'https://tululu.org/b{}/'.format(i)
     response = requests.get(url)
     try:
@@ -54,35 +92,12 @@ for i in range(1, 11):
         continue
     response.raise_for_status()
 
-    soup = BeautifulSoup(response.text, 'lxml')
-    info_text = soup.find('h1').text
-    filename = info_text.split('::')[0].strip()
-    print('Заголовок:', filename)
-    genres_list = []
-    genres = soup.find('span', class_='d_book').find_all('a')
-    for genre in genres:
-        genres_list.append(genre.text)
-    print(genres_list)
-    # img_url = soup.find(class_='bookimage').find('img')['src']
-    # img_abs_url = urljoin(url, img_url)
+    book_info = parse_book_page(response.text)
+    print('Заголовок:', book_info['title'])
+    print('Автор:', book_info['author'])
     print()
 
-    '''comments = soup.find_all('div', class_='texts')
-    for comment in comments:
-        comment_text = comment.find('span', class_='black').text
-        print(comment_text)'''
-
-
-    '''info_text = soup.find('h1').text
-    filename = info_text.split('::')[0].strip()
-
-    img_url = soup.find(class_='bookimage').find('img')['src']
-    img_abs_url = urljoin(url, img_url)
-
-    print('Заголовок:', filename)
-    print(img_abs_url)
-    print(download_image(img_abs_url))
-    print()'''
-
-    '''url = 'https://tululu.org/txt.php?id={}'.format(i)
-    download_txt(url, filename=filename)'''
+    img_url = urljoin(url, book_info['img_url'])
+    download_image(img_url)
+    url = 'https://tululu.org/txt.php?id={}'.format(i)
+    download_txt(url, filename=book_info['title'])
