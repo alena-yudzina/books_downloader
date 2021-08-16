c8606ce07f98a3dddbd4191c76d2a2a57d1fc0a9
alena-yudzina <lenok.ok.lol@gmail.com>
2021-08-16 14:21:48 +0300

    refactor download.py

--- download.py
+++ download.py
@@ -3,8 +3,9 @@ import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
-from urllib.parse import urlparse, urlsplit, urljoin
+from urllib.parse import urlparse, urlsplit, urljoin, unquote
 import argparse
+import datetime
 
 
 def parse_cli_args():
@@ -17,28 +18,21 @@ def parse_cli_args():
 
 
 def parse_book_page(page):
-    book_info = {}
     soup = BeautifulSoup(page, 'lxml')
     title_and_author = soup.find('h1').text
-    book_info['title'] = title_and_author.split('::')[0].strip()
-    book_info['author'] = title_and_author.split('::')[1].strip()
-
+    title, author = title_and_author.split('::')
     genres = soup.find('span', class_='d_book').find_all('a')
-    genres_list = []
-    for genre in genres:
-        genres_list.append(genre.text)
-    book_info['genre'] = genres_list
-
+    genres = [genre.text for genre in genres]
     img_url = soup.find(class_='bookimage').find('img')['src']
-    book_info['img_url'] = img_url
-
     comments = soup.find_all('div', class_='texts')
-    comments_list = []
-    for comment in comments:
-        comment_text = comment.find('span', class_='black').text
-        comments_list.append(comment_text)
-    book_info['comments'] = comments_list
-
+    comments = [comment.find('span', class_='black').text for comment in comments]
+    book_info = {
+        'title': title.strip(),
+        'author': author.strip(),
+        'genre': genres,
+        'img_url': img_url,
+        'comments': comments
+    }
     return book_info
 
 
@@ -47,57 +41,68 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_txt(url, filename, folder='books/'):
+def download_txt(response, filename, folder='books/'):
 
-    response = requests.get(url)
-    try:
-        check_for_redirect(response)
-    except requests.HTTPError:
-        return
-    response.raise_for_status()
     Path(folder).mkdir(parents=True, exist_ok=True)
-    filename = '{}.txt'.format(sanitize_filename(filename))
-    with open(Path(folder) / filename, 'wb') as file:
-        file.write(response.content)
+    timestamp = datetime.datetime.now().strftime('%y%m%d_%H%M%S')
+    filename = '{}_{}.txt'.format(timestamp, sanitize_filename(filename))
     filepath = Path(folder) / filename
-    
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
     return filepath
 
 
 def download_image(url, folder='images'):
 
     response = requests.get(url)
-    try:
-        check_for_redirect(response)
-    except requests.HTTPError:
-        return
-    response.raise_for_status()
     Path(folder).mkdir(parents=True, exist_ok=True)
-    url_path = urlparse(url).path
-    filename = url_path.rsplit('/', 1)[-1]
-    filename = sanitize_filename(filename)
-    with open(Path(folder) / filename, 'wb') as file:
-        file.write(response.content)
+    url_path = urlsplit(url).path
+    filename = unquote(Path(url_path).name)
+    timestamp = datetime.datetime.now().strftime('%y%m%d_%H%M%S')
+    filename = '{}_{}'.format(timestamp, sanitize_filename(filename))
     filepath = Path(folder) / filename
-    
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
     return filepath
 
-args = parse_cli_args()
-for i in range(args.start_id, args.end_id + 1):
-    url = 'https://tululu.org/b{}/'.format(i)
-    response = requests.get(url)
-    try:
-        check_for_redirect(response)
-    except requests.HTTPError:
-        continue
-    response.raise_for_status()
 
-    book_info = parse_book_page(response.text)
-    print('Заголовок:', book_info['title'])
-    print('Автор:', book_info['author'])
-    print()
+def main():
+    args = parse_cli_args()
+    for book_id in range(args.start_id, args.end_id + 1):
+        url = 'https://tululu.org/b{}/'.format(book_id)
+        response = requests.get(url)
+        try:
+            check_for_redirect(response)
+            response.raise_for_status()
+        except requests.HTTPError:
+            continue
 
-    img_url = urljoin(url, book_info['img_url'])
-    download_image(img_url)
-    url = 'https://tululu.org/txt.php?id={}'.format(i)
-    download_txt(url, filename=book_info['title'])
+        book_info = parse_book_page(response.text)
+        print('Заголовок:', book_info['title'])
+        print('Автор:', book_info['author'])
+        print()
+
+        img_url = urljoin(url, book_info['img_url'])
+        response = requests.get(url)
+        try:
+            check_for_redirect(response)
+            response.raise_for_status()
+        except requests.HTTPError:
+            return
+        download_image(img_url)
+
+        url = 'https://tululu.org/txt.php'
+        payload = {'id': str(book_id)}
+        response = requests.get(url, params=payload)
+        try:
+            check_for_redirect(response)
+            response.raise_for_status()
+        except requests.HTTPError:
+            return
+        download_txt(response, filename=book_info['title'])
+
+
+if __name__ == '__main__':
+    main()
